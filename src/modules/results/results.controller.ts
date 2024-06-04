a5386af2cbcb05eae121a6c5cb9007058b5b9604
MiguelTabares <silenceisbeautyofsoul@gmail.com>
2024-06-04 10:11:14 -0500

    Resultscontroller implemented

--- src/modules/results/results.controller.ts
+++ src/modules/results/results.controller.ts
@@ -1,30 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { ResultsService } from './results.service';
-import { CreateResultDto } from './dto/create-result.dto';
-import { UpdateResultDto } from './dto/update-result.dto';
+import { CreateResultDto, UpdateResultDto } from '../../shared/dtos/index-dtos';
+import { Result } from 'src/shared/entities/result.entity';
 
 @Controller('results')
 export class ResultsController {
   constructor(private readonly resultsService: ResultsService) {}
 
   @Post()
-  create(@Body() createResultDto: CreateResultDto) {
-    return this.resultsService.create(createResultDto);
+  createResult(
+    @Body('playerId') playerId: number,
+    @Body('tournamentId') tournamentId: number,
+    @Body() createResultDto: CreateResultDto,
+  ) {
+    return this.resultsService.createResult(
+      playerId,
+      tournamentId,
+      createResultDto,
+    );
   }
 
   @Get()
-  findAll() {
-    return this.resultsService.findAll();
+  getResultsByPlayer(
+    @Query('playerId') playerId: number,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('sortField') sortField: 'score',
+    @Query('sortOrder') sortOrder: 'asc' | 'desc' = 'desc',
+  ): Promise<[Result[], number]>{
+    return this.resultsService.getResultsByPlayer(
+      playerId,
+      page,
+      limit,
+      sortField,
+      sortOrder,
+    );
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.resultsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateResultDto: UpdateResultDto) {
-    return this.resultsService.update(+id, updateResultDto);
   }
 
   @Delete(':id')
